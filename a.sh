dd36ff1d370bfff9b6cd58ef0efbb12f292815fb
zf <bingxinhu@qq.com>
2023-09-11 16:51:52 +0800

    Runing version epoch3

---
+++ a.sh
@@ -0,0 +1,12 @@
+python3 scripts/view_data.py \
+  data=nuscenes \
+  data.dataset_dir=/home/zhaifang/transformer/data/nuscenes \
+  data.labels_dir=/home/zhaifang/transformer/data/cvt_labels_nuscenes \
+  data.version=/home/zhaifang/transformer/data/nuscenes/v1.0-trainval \
+  visualization=nuscenes_viz \
+  +split=val
+  
+
+
+
+
---
+++ t.sh
@@ -0,0 +1,4 @@
+  python3 scripts/train.py \
+  +experiment=cvt_nuscenes_vehicle
+  data.dataset_dir=/home/zhaifang/transformer/data/nuscenes \
+  data.labels_dir=/home/zhaifang/transformer/data/cvt_labels_nuscenes  config/config.yaml 
