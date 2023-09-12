583beae69713378974150f1739bc43678dc54c22
zf <bingxinhu@qq.com>
2023-09-12 13:23:13 +0800

    Run model prediction

---
+++ c.sh
@@ -0,0 +1,8 @@
+python3 scripts/generate_data.py \
+    data=nuscenes \
+    data.version=/home/zhaifang/transformer/data/nuscenes/v1.0-trainval \
+    data.dataset_dir=/home/zhaifang/transformer/data/nuscenes \
+    data.labels_dir=/home/zhaifang/transformer/data/cvt_labels_nuscenes \
+    visualization=nuscenes_viz
+
+
